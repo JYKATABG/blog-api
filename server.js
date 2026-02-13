39b80f81250bfbde082fb489bc61f8d43ecc42b0
Jykata <jykatabg@gmail.com>
2026-02-14 01:14:58 +0200

    Added rate limiting to post, user and auth routes

--- server.js
+++ server.js
@@ -4,13 +4,41 @@ import authRouter from "./routes/authRouter.js";
 import postRouter from "./routes/postRouter.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import { HttpError } from "./utils/HttpError.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {
+        error: "Too many requests from this IP, please try again later",
+        retryAfter: "15 mins"
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: {
+        error: "Too many login attempts, please try again later",
+        retryAfter: "10 mins"
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+    skipSuccessfulRequests: true,
+    skipFailedRequests: false
+})
+
 app.use(express.json());
 
+app.use("/api", apiLimiter)
+
+app.use("/api/auth", authLimiter, authRouter);
+
 app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
 app.use((req, res, next) => {
