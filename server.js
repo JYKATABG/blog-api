bd7a4c6909be246e94fad8ac353937db7f312de4
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-02-15 21:24:35 +0200

    Add: Shows route and method for every api call

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import userRouter from "./routes/userRouter.js";
 import authRouter from "./routes/authRouter.js";
 import postRouter from "./routes/postRouter.js";
@@ -9,32 +11,37 @@ import rateLimit from "express-rate-limit";
 const app = express();
 
 const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: {
-        error: "Too many requests from this IP, please try again later",
-        retryAfter: "15 mins"
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Too many requests from this IP, please try again later",
+    retryAfter: "15 mins",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 5,
-    message: {
-        error: "Too many login attempts, please try again later",
-        retryAfter: "10 mins"
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-    skipSuccessfulRequests: true,
-    skipFailedRequests: false
-})
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    error: "Too many login attempts, please try again later",
+    retryAfter: "10 mins",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  skipSuccessfulRequests: true,
+  skipFailedRequests: false,
+});
 
 app.use(express.json());
 
-app.use("/api", apiLimiter)
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.path}`);
+  next();
+});
+
+app.use("/api", apiLimiter);
 
 app.use("/api/auth", authLimiter, authRouter);
 
@@ -42,11 +49,17 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 
 app.use((req, res, next) => {
-    throw new HttpError(`Route ${req.originalUrl} not found`, 404, 'ROUTE_NOT_FOUND');
-})
+  throw new HttpError(
+    `Route ${req.originalUrl} not found`,
+    404,
+    "ROUTE_NOT_FOUND",
+  );
+});
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Server is running");
-})
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`,
+  );
+});
